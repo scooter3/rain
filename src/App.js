b6f1d391efddffeca16b60ccf87d9593da232797
Philemon Lin <philemonlin@gmail.com>
2019-04-06 16:02:17 -0400

    added description and formatted

--- src/App.js
+++ src/App.js
@@ -27,44 +27,47 @@ class App extends Component {
   componentDidMount() {
     this.getDate();
     navigator.geolocation.getCurrentPosition(position => {
-      this.getCityName(
-        position.coords.latitude,
-        position.coords.longitude
-      ).then(result => {
-        this.setState({
-          location: result[0]
-        });
-      }).then(() => {
-        this.weather.setZipCode(this.state.location.zipcode);
-        // fetch weather data
-        this.getWeatherData().then(response => {
-          console.log(JSON.stringify(response, null, 2));
+      this.getCityName(position.coords.latitude, position.coords.longitude)
+        .then(result => {
           this.setState({
-            weather: {
-              currentWeather: response
-            }
+            location: result[0]
+          });
+        })
+        .then(() => {
+          this.weather.setZipCode(this.state.location.zipcode);
+          // fetch weather data
+          this.getWeatherData().then(response => {
+            console.log(JSON.stringify(response, null, 2));
+            this.setState({
+              weather: {
+                currentWeather: response
+              }
+            });
+          });
+
+          // fetch forecast data
+          this.getForecast().then(response => {
+            this.setState({
+              forecast: response
+            });
           });
         });
-    
-        // fetch forecast data
-        this.getForecast().then((response) => {
-          this.setState({
-            forecast: response
-          });
-        });
-      });
     });
   }
 
   /**
    * Get the current date.
-   * 
+   *
    */
   getDate() {
     let moment = require("moment");
     let fiveDays = [];
-    for(let i=1; i < 6; i++) {
-        fiveDays.push(moment().add(i, 'days').format("dddd"));
+    for (let i = 1; i < 6; i++) {
+      fiveDays.push(
+        moment()
+          .add(i, "days")
+          .format("dddd")
+      );
     }
 
     this.setState({
@@ -79,15 +82,15 @@ class App extends Component {
 
   /**
    * Get new weather data for the given zip code.
-   * 
+   *
    */
-  updateAll = (zipCode) => {
+  updateAll = zipCode => {
     // update date
     this.getDate();
 
     // update zip code
     this.setState({
-      location:{
+      location: {
         zipcode: zipCode
       }
     });
@@ -100,32 +103,34 @@ class App extends Component {
     let NodeGeocoder = require("node-geocoder");
     let geocoder = NodeGeocoder(options);
 
-    geocoder.geocode({'address':zipCode})
-    .then((result) => {
-      this.setState({
-        location: result[0]
+    geocoder
+      .geocode({ address: zipCode })
+      .then(result => {
+        this.setState({
+          location: result[0]
+        });
       })
-    }).catch((err) => {
-      console.log("ERROR: " + err);
-    });
+      .catch(err => {
+        console.log("ERROR: " + err);
+      });
 
     // update weather
     this.weather.setZipCode(zipCode);
-    this.getWeatherData().then(response =>{
+    this.getWeatherData().then(response => {
       this.setState({
         weather: {
           currentWeather: response
         }
-      })
+      });
     });
 
     // update forecast
-    this.getForecast().then((response) => {
+    this.getForecast().then(response => {
       this.setState({
         forecast: response
       });
     });
-  }
+  };
 
   /**
    * Get weather data from OpenWeather.
@@ -144,9 +149,9 @@ class App extends Component {
 
   /**
    * Get forecast data given zip code.
-   * 
+   *
    */
-  getForecast = (zipCode) => {
+  getForecast = zipCode => {
     return new Promise((resolve, reject) => {
       this.weather.getWeatherForecastForDays(this.weatherForecastDays, function(
         err,
@@ -162,7 +167,7 @@ class App extends Component {
 
   /**
    * Get the city name with the given latitude and longitude.
-   * 
+   *
    */
   getCityName = (latitude, longitude) => {
     let options = {
@@ -195,36 +200,43 @@ class App extends Component {
 
   /**
    * Determine part of the day given the time (day or night)
-   * 
+   *
    */
   getPartOfDay = () => {
     let moment = require("moment");
     let m = moment();
     let partOfDay;
-    
-    if(!m || !m.isValid()) { return; } //if we can't find a valid or filled moment, we return.
-    
-    var split_afternoon = 12 //24hr time to split the afternoon
-    var split_evening = 15 //24hr time to split the evening
+
+    if (!m || !m.isValid()) {
+      return;
+    } //if we can't find a valid or filled moment, we return.
+
+    var split_afternoon = 12; //24hr time to split the afternoon
+    var split_evening = 15; //24hr time to split the evening
     var currentHour = parseFloat(m.format("HH"));
-    
-    if(currentHour >= split_afternoon && currentHour <= split_evening) {
+
+    if (currentHour >= split_afternoon && currentHour <= split_evening) {
       partOfDay = "afternoon";
-    } else if(currentHour >= split_evening) {
+    } else if (currentHour >= split_evening) {
       partOfDay = "evening";
     } else {
       partOfDay = "morning";
     }
-    
+
     return partOfDay;
-  }
+  };
 
   render() {
     if (this.state.weather && this.state.forecast) {
       return (
         <div className="App">
           {/* <Zip updateAll={this.updateAll}></Zip> */}
-          <Weather weather={this.state.weather} location={this.state.location} forecast={this.state.forecast} date={this.state.date}/>
+          <Weather
+            weather={this.state.weather}
+            location={this.state.location}
+            forecast={this.state.forecast}
+            date={this.state.date}
+          />
         </div>
       );
     }
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,70 +1,77 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import {Skycons} from '.././skycons';
-import moment from 'moment';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Skycons } from ".././skycons";
+import moment from "moment";
 
 class Weather extends Component {
-
   constructor(props) {
     super(props);
     this.forecastIcons = [];
-    this.skycons = new Skycons({"color": "white"});
+    this.skycons = new Skycons({ color: "white" });
   }
 
   componentDidMount() {
     // Render main weather icon
-    const {currentWeather} = this.props.weather;
-    const {date} = this.props;
-    const weatherIcon = this.getWeatherType(currentWeather.weather[0].id, date.part);
+    const { currentWeather } = this.props.weather;
+    const { date } = this.props;
+    const weatherIcon = this.getWeatherType(
+      currentWeather.weather[0].id,
+      date.part
+    );
 
     this.skycons.set(document.getElementById("mainWeatherIcon"), weatherIcon);
     this.skycons.play();
 
     // Render forecast icons
-    for(let i=0; i < 5; i++) {
-      this.skycons.set(document.getElementById("forecast" + i), this.forecastIcons[i])
+    for (let i = 0; i < 5; i++) {
+      this.skycons.set(
+        document.getElementById("forecast" + i),
+        this.forecastIcons[i]
+      );
     }
   }
 
-  getWeatherType(weatherCode, partOfDay = 'morning') {
+  getWeatherType(weatherCode, partOfDay = "morning") {
     const x = weatherCode;
 
-    switch(true){
+    switch (true) {
       // thunderstorms
-      case (x >= 200 && x < 300):
+      case x >= 200 && x < 300:
         return Skycons.RAIN;
-      
+
       // drizzle
-      case(x >=300 && x < 400):
+      case x >= 300 && x < 400:
         return Skycons.RAIN;
 
       // rain
-      case(x >= 500 && x < 600):
+      case x >= 500 && x < 600:
         return Skycons.RAIN;
 
-      case(x === 611 || x === 612):
+      case x === 611 || x === 612:
         return Skycons.SLEET;
 
-      case(x >= 600 && x < 700):
+      case x >= 600 && x < 700:
         return Skycons.SNOW;
-      
-      case(x === 741):
+
+      case x === 741:
         return Skycons.FOG;
 
-      case(x === 800 && (partOfDay === 'afternoon' || partOfDay === 'morning')):
+      case x === 800 && (partOfDay === "afternoon" || partOfDay === "morning"):
         return Skycons.CLEAR_DAY;
 
-      case(x === 800 && partOfDay === 'evening'):
+      case x === 800 && partOfDay === "evening":
         return Skycons.CLEAR_NIGHT;
 
       // check for 804 (overcast clouds) first because it has a unique icon
-      case(x === 804):
+      case x === 804:
         return Skycons.CLOUDY;
-      
-      case((x >= 801 && x < 900) && (partOfDay === 'afternoon' || partOfDay === 'morning')):
+
+      case x >= 801 &&
+        x < 900 &&
+        (partOfDay === "afternoon" || partOfDay === "morning"):
         return Skycons.PARTLY_CLOUDY_DAY;
 
-      case((x >= 801 && x < 900) && partOfDay === 'evening'):
+      case x >= 801 && x < 900 && partOfDay === "evening":
         return Skycons.PARTLY_CLOUDY_NIGHT;
 
       default:
@@ -73,67 +80,91 @@ class Weather extends Component {
   }
 
   render() {
-    const {currentWeather} = this.props.weather;
-    const {location, date, forecast} = this.props;
+    const { currentWeather } = this.props.weather;
+    const { location, date, forecast } = this.props;
     let moment = require("moment");
-    
-    forecast.list.forEach((element) => {
+
+    forecast.list.forEach(element => {
       this.forecastIcons.push(this.getWeatherType(element.weather[0].id));
     });
-    
+
     return (
       <div className="Weather">
-
         <div id="mainWeatherArea">
-
           <div class="weatherColumn">
-            <div id="mainWeatherIconBox"><canvas id="mainWeatherIcon" width="128" height="128" title={currentWeather.weather[0].main}></canvas></div>
+            <div id="mainWeatherIconBox">
+              <canvas
+                id="mainWeatherIcon"
+                width="128"
+                height="128"
+                title={currentWeather.weather[0].main}
+              />
+            </div>
           </div>
 
           <div class="weatherColumn">
-            <div id="mainTemp">{Number.parseFloat(currentWeather.main.temp).toFixed(0)}&deg;</div>
+            <div id="mainTemp">
+              {Number.parseFloat(currentWeather.main.temp).toFixed(0)}&deg;
+            </div>
+            <div id="description">{currentWeather.weather[0].description}</div>
             <div id="highLow">
-              H {Number.parseFloat(currentWeather.main.temp_max).toFixed(0)}&deg; L {Number.parseFloat(currentWeather.main.temp_min).toFixed(0)}&deg;
+              H {Number.parseFloat(currentWeather.main.temp_max).toFixed(0)}
+              &deg; L{" "}
+              {Number.parseFloat(currentWeather.main.temp_min).toFixed(0)}&deg;
             </div>
           </div>
 
           <div class="weatherColumn">
             <div id="info">
-              <div id="cityState">{location.city}, {location.administrativeLevels.level1short}</div>
-              <div id="date">{date.day} {date.full}</div>
-              <div id="time">as of {moment.unix(currentWeather.dt).format("h:mm A")}</div>
+              <div id="cityState">
+                {location.city}, {location.administrativeLevels.level1short}
+              </div>
+              <div id="date">
+                {date.day} {date.full}
+              </div>
+              <div id="time">
+                as of {moment.unix(currentWeather.dt).format("h:mm A")}
+              </div>
             </div>
 
             <div id="otherWeatherData">
               <p>
                 Humidity: {currentWeather.main.humidity}% <br />
-                Pressure: {Number.parseFloat(currentWeather.main.pressure * 0.0145037738).toFixed(2)} in<br />
+                Pressure:{" "}
+                {Number.parseFloat(
+                  currentWeather.main.pressure * 0.0145037738
+                ).toFixed(2)}{" "}
+                in
+                <br />
                 Wind Speed: {currentWeather.wind.speed} MPH <br />
-                Sun: {moment.unix(currentWeather.sys.sunrise).format("h:mm A")} | {moment.unix(currentWeather.sys.sunset).format("h:mm A")}
+                Sun: {moment
+                  .unix(currentWeather.sys.sunrise)
+                  .format("h:mm A")}{" "}
+                | {moment.unix(currentWeather.sys.sunset).format("h:mm A")}
               </p>
             </div>
           </div>
-
         </div>
 
-          {forecast.list.map(function(value, index) {
-            return (
-              <div id="forecastTable">
-                <div class="forecastDay">{date.fiveDays[index]}</div>
-                <div class="forecastIcon"><canvas width="30" height="30" id={"forecast" + index} title={value.weather[0].main}></canvas></div>
-                <div class="forecastTemp">{Number.parseFloat(value.temp.max).toFixed(0)}&deg; | {Number.parseFloat(value.temp.min).toFixed(0)}&deg;</div>
-                {/* <div>{value.weather[0].main}</div> */}
-                
+        {forecast.list.map(function(value, index) {
+          return (
+            <div id="forecastTable">
+              <div class="forecastDay">{date.fiveDays[index]}</div>
+              <div class="forecastIcon">
+                <canvas
+                  width="30"
+                  height="30"
+                  id={"forecast" + index}
+                  title={value.weather[0].main}
+                />
               </div>
-            )
-          })}
-        {/* <pre>{JSON.stringify(currentWeather, null, 2)}</pre>
-        <br/>
-        <br/>
-        <pre>{JSON.stringify(currentWeather.weather[0], null, 2)}</pre>
-        <br/>
-        <br/>
-        <pre>{JSON.stringify(forecast, null, 2)}</pre> */}
+              <div class="forecastTemp">
+                {Number.parseFloat(value.temp.max).toFixed(0)}&deg; |{" "}
+                {Number.parseFloat(value.temp.min).toFixed(0)}&deg;
+              </div>
+            </div>
+          );
+        })}
       </div>
     );
   }
@@ -142,6 +173,6 @@ class Weather extends Component {
 Weather.propTypes = {
   weather: PropTypes.object.isRequired,
   forecast: PropTypes.object.isRequired
-}
+};
 
 export default Weather;
